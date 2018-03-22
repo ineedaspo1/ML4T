c7e072b5302447819e7bdb19d4196fd1425af745
truzmeto <truzmeto@kent.edu>
2018-03-22 12:11:12 -0400

    indicators

--- manual_strategy/indicators.py
+++ manual_strategy/indicators.py
@@ -12,66 +12,47 @@ import datetime as dt
 from util import get_data, plot_data
 
 
-def indicators(sd = dt.datetime(2008,1,1), ed = dt.datetime(2009,1,1), \
-    syms = ['JPM'], \
-    sv=100000, rfr=0.0, sf=252.0):
+def indicators(sd = dt.datetime(2008,1,1), ed = dt.datetime(2009,12,31), \
+    syms = ['JPM'], sv=100000):
 
     # Read in adjusted closing prices for given symbols, date range
     dates = pd.date_range(sd, ed)
     prices_all = get_data(syms, dates)  # automatically adds SPY
-    prices = prices_all[syms]           # only portfolio symbols
+    price = prices_all[syms]           # only portfolio symbols
    
-    # Get daily portfolio value
-    normed = prices.divide(prices.ix[0])
-    #allocated = normed * allocs 
-    pos_vals = normed * sv 
-    port_val = pos_vals.sum(axis = 1)
+    #calc bb value
+    window_size = 20
+    k = 2
+    sma = price.rolling(window_size).mean()
+    std_bb = price.rolling(window_size).std()
+    bb = (price - sma) / (2*std_bb)
+
+    # calc momentum
+    delta = 5
+    momentum = price/price.shift(delta) - 1
     
-    # get cumulative return
-    cr = (port_val[-1] -port_val[0]) / port_val[0] 
-
-    # get daily return
-    daily_r = port_val / port_val.shift(1) - 1.00
-
-    # average daily return
-    adr = daily_r.mean()
-
-    # calc. std for daily return
+    
+    #get sddr = volatility
+    daily_r = price / price.shift(1) - 1.00
     sddr = daily_r.std()
-
-    # get sharp ratio
-    sr = np.sqrt(sf) * (adr - rfr) / sddr         
-   
-    return cr, adr, sddr, sr
-
-
-def sd_daily_r(allocs, normed):
-    """ This function calculates standart deviation for daily return """
-    allocated = normed * allocs 
-    pos_vals = allocated  
-    port_val = pos_vals.sum(axis = 1)
-    daily_r = port_val / port_val.shift(1) - 1.00000
-    sddr = daily_r.std()
-    return sddr
+    vol = sddr.values()
+    
+    return bb, std_bb, momentum, vol
 
 
 def test_code():
-    start_date = dt.datetime(2008,6,1)
-    end_date = dt.datetime(2009,6,1)
-    symbols = ['IBM','X','GLD']
-    allocations, cr, adr, sddr, sr = optimize_portfolio(sd = start_date, ed = end_date,\
-        syms = symbols, \
-        gen_plot = False)
+    start_date = dt.datetime(2008,1,1)
+    end_date = dt.datetime(2009,12,31)
+    symbols = ['JPM']
+    sv = 100000
+    bb, std_bb, momentum, vol = indicators(sd = start_date, ed = end_date, syms = symbols, sv = sv)
 
-    # Print statistics
-    print "Start Date:", start_date
-    print "End Date:", end_date
-    print "Symbols:", symbols
-    print "Allocations:", allocations
-    print "Sharpe Ratio:", sr
-    print "Volatility (stdev of daily returns):", sddr
-    print "Average Daily Return:", adr
-    print "Cumulative Return:", cr
+    df_temp = pd.concat([port_val / port_val[0],\
+                         prices_SPY / prices_SPY.iloc[0]],\
+                        keys = ['Portfolio', 'SPY'],\
+                        axis = 1)
+    plot_data(df_temp)
 
+   
 if __name__ == "__main__":
     test_code()
