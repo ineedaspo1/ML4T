5e39ee75be1f651f486d337bfb216e56e36a2cc1
truzmeto <truzmeto@kent.edu>
2018-03-27 13:58:47 -0400

    BPS done, ready to write

--- manual_strategy/ManualStrategy.py
+++ manual_strategy/ManualStrategy.py
@@ -1,4 +1,4 @@
-"""  Manual Strategy
+"""  Manual Strategy by T. Ruzmetov
 
 Copyright 2018, Georgia Tech Research Corporation
 Atlanta, Georgia 30332-0415
@@ -21,7 +21,7 @@ def testPolicy(symbol = "JPM", sd=dt.datetime(2008,1,1), ed=dt.datetime(2009,12,
                sv = 100000, rolling_days = 20, k = 2 ):
     """
     This function uses indicators to generate orders data frame based on manual strategy.
-    
+    --------------------------
     Parameters
     k -  rolling_std prefactor for bb calculation
     rolling_days - number of rolling days for rolling_mean and r_std calculation
@@ -32,28 +32,23 @@ def testPolicy(symbol = "JPM", sd=dt.datetime(2008,1,1), ed=dt.datetime(2009,12,
     prices = prices_all[syms]  # only portfolio symbols
     prices_normalized = normalize_stocks(prices)
 
-    #calc SMA 
     sma = compute_sma(prices_normalized, rolling_days)
-   
-    #calc bollinger bands 
     bb = compute_bollinger_bands(prices_normalized, rolling_days, sma, k)
-   
-    #calc momentum
+
     m_rd = 5
-    momentum = compute_momentum(prices_normalized, rolling_days = m_rd)
+    momentum = compute_momentum(prices_normalized, rd = m_rd)
 
     df_sha = pd.DataFrame(0, index = prices_normalized.index, columns = ['Shares'])
     df_ord = pd.DataFrame('BUY', index = prices_normalized.index, columns = ['Order'])
     df_sym = pd.DataFrame(symbol, index = prices_normalized.index, columns = ['Symbol'])
     df_mom = pd.DataFrame(momentum, index = prices_normalized.index, columns = ['Momentum'])
     net_holdings = 0
-
     #loop over rows to generate signals for trading
     for i, row in prices_normalized.iterrows():
         sma_value = sma.loc[i]['SMA']
         upper_b = bb.loc[i]['upper']
         lower_b = bb.loc[i]['lower']
-        #mom = df_mom.loc[i]['Momentum']
+        mom = df_mom.loc[i]['Momentum']
         price = row[symbol]
 
         if (price < lower_b) and (net_holdings < 1000):
@@ -73,29 +68,33 @@ def testPolicy(symbol = "JPM", sd=dt.datetime(2008,1,1), ed=dt.datetime(2009,12,
                 df_sha.loc[i] = 2000
                 net_holdings -= 2000
 
-    df = pd.concat([df_sym, df_ord, df_sha], axis=1)
+    df = pd.concat([df_sym, df_ord, df_sha], axis=1) # merge all and make orders
     df.columns = ['Symbol', 'Order', 'Shares']
     df = df[df.Shares != 0] # drop rows with zero Shares
     return df
 
 
-def compute_sma(prices_normed, rolling_days):
-    sma = pd.DataFrame(0, index = prices_normed.index, columns = ['SMA'])
-    sma['SMA'] = prices_normed.rolling(window=rolling_days, min_periods = rolling_days).mean()
+def compute_sma(price, rd):
+    """ Compute rolling mean given normalized price and  #of_rolling_days=rd"""
+    sma = pd.DataFrame(0, index = price.index, columns = ['SMA'])
+    sma['SMA'] = price.rolling(window=rd, min_periods = rd).mean()
     return sma
 
-def compute_bollinger_bands(prices_normed, rolling_days, sma, k=2):
-    bb = pd.DataFrame(0, index = prices_normed.index, columns = ['lower','upper'])
-    bands = pd.DataFrame(0, index = prices_normed.index, columns = ['band'])
-    bands['band'] = prices_normed.rolling(window = rolling_days, min_periods = rolling_days).std()
-    bb['upper'] = sma['SMA'] + (bands['band'] * k)
-    bb['lower'] = sma['SMA'] - (bands['band'] * k)
+def compute_bollinger_bands(price, rd, sma, k=2):
+    """Calculated lower and upper Bollinger Ban """
+    bb = pd.DataFrame(0, index = price.index, columns = ['lower','upper'])
+    roll_std = pd.DataFrame(0, index = price.index, columns = ['band'])
+    roll_std['band'] = price.rolling(window = rd, min_periods = rd).std()
+    bb['upper'] = sma['SMA'] + roll_std['band'] * k
+    bb['lower'] = sma['SMA'] - roll_std['band'] * k
+    
     return bb
 
-def compute_momentum(prices_normed, rolling_days):
-    momentum = pd.DataFrame(0, index = prices_normed.index, columns = ['Momentum'])
-    momentum['Momentum'] = prices_normed.diff(rolling_days)/prices_normed.shift(rolling_days)
-    return momentum
+def compute_momentum(price, rd):
+    """ Compute Momentum given normalized prices and  #of_rolling_days=rd"""
+    m = pd.DataFrame(0, index = price.index, columns = ['Momentum'])
+    m['Momentum'] = price.diff(rd)/price.shift(rd)
+    return m
 
 def normalize_stocks(prices):
     fill_missing_values(prices)
@@ -144,30 +143,39 @@ def test_code():
     prices_portval_normalized = normalize_stocks(portvals)
     prices =  prices_all[syms]
     prices_JPM_normalized = normalize_stocks(prices)
+
+    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = ms.compute_portfolio_stats(portvals)
+    cum_ret_Bench, avg_daily_ret_Bench, std_daily_ret_Bench, sharpe_ratio_Bench = ms.compute_portfolio_stats(prices)
+
+    print('In Sample stats:')
+    print "Average Daily Return for Portfolio: {}".format(avg_daily_ret)
+    print "Average Daily Return for Benchmark: {}".format(avg_daily_ret_Bench)
+    print "Cumulative Return of Portfolio : {}".format(cum_ret)
+    print "Cumulative Return of Benchmark : {}".format(cum_ret_Bench)
+    print "Std for Daily Return of Portfolio : {}".format(std_daily_ret)
+    print "Std for Daily Return of Benchmark : {}".format(std_daily_ret_Bench)
     
+    ###########################################################################################
+    #plot
     chart_df = pd.concat([prices_portval_normalized, prices_JPM_normalized], axis=1)
     chart_df.columns = ['Portfolio', 'Benchmark']
-    chart_df.plot(title='Manual Strategy and Benchmark Comparison for In Sample', use_index=True, color=['Black', 'Blue'])
+    chart_df.plot(title='Manual Strategy and Benchmark Comparison for In Sample',\
+                  use_index=True,\
+                  color=['Black', 'Blue'],\
+                  figsize=(10,5))
 
     for index, row in df_trades.iterrows():
         if df_trades.loc[index]['Order'] == 'BUY':
             plt.axvline(x=index, color='g', linestyle='-')
         elif df_trades.loc[index]['Order'] == 'SELL':
             plt.axvline(x=index, color='r', linestyle='-')
-            
-    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = ms.compute_portfolio_stats(portvals)
-    cum_ret_Bench, avg_daily_ret_Bench, std_daily_ret_Bench, sharpe_ratio_Bench = ms.compute_portfolio_stats(prices)
 
-    print('In Sample stats:')
-    print
-    print "Cumulative Return of Fund: {}".format(cum_ret)
-    print "Cumulative Return of Benchmark : {}".format(cum_ret_Bench)
-    print
-    print "Final Portfolio Value: {}".format(portvals[-1])
-    
-    print('#################################################################################')
+    #plt.set_tight_layout(True)
+    plt.show()
+    #plt.savefig('plots/MS_InSample.pdf')
+    #############################################################################################
+     
 
-    print('Out of Sample Stats:')
     sd=dt.datetime(2010,1,1)
     ed=dt.datetime(2011,12,31)
     sv = 100000
@@ -180,20 +188,31 @@ def test_code():
     prices_portval_normalized = normalize_stocks(portvals)
     prices =  prices_all[syms]
     prices_JPM_normalized = normalize_stocks(prices)
-
     
     cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = ms.compute_portfolio_stats(portvals)
     cum_ret_Bench, avg_daily_ret_Bench, std_daily_ret_Bench, sharpe_ratio_Bench = ms.compute_portfolio_stats(prices)
 
+    print('#################################################################################')
+    print('Out of Sample Stats:')
+    print "Average Daily Return for Portfolio : {}".format(avg_daily_ret)
+    print "Average Daily Return for Benchmark: {}".format(avg_daily_ret_Bench)
+    print "Cumulative Return of Portfolio : {}".format(cum_ret)
+    print "Cumulative Return of Benchmark : {}".format(cum_ret_Bench)
+    print "Std for Daily Return of Portfolio : {}".format(std_daily_ret)
+    print "Std for Daily Return of Benchmark : {}".format(std_daily_ret_Bench)
+
+    ###########################################################################################
+    #plot
     chart_df = pd.concat([prices_portval_normalized, prices_JPM_normalized], axis=1)
     chart_df.columns = ['Portfolio', 'Benchmark']
-    chart_df.plot(title='Manual Strategy and Benchmark Comparison for Out of Sample', use_index=True, color=['Black', 'Blue'])
-
-    print "Cumulative Return of Fund: {}".format(cum_ret)
-    print "Cumulative Return of Benchmark : {}".format(cum_ret_Bench)
-    print "Final Portfolio Value: {}".format(portvals[-1])
-    
+    chart_df.plot(title='Manual Strategy and Benchmark Comparison for Out of Sample',\
+                  use_index=True,\
+                  color=['Black', 'Blue'],\
+                  figsize=(10,5))
+    #plt.set_tight_layout(True)
     plt.show()
-
+    #plt.savefig('plots/MS_OutSample.png')
+    ###########################################################################################
+    
 if __name__ == "__main__":
     test_code()
