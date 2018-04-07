fc5af4cf036d8904a69bda4ad0777b7e5b93c77f
truzmeto <truzmeto@kent.edu>
2018-04-07 18:57:45 -0400

    query part done

--- qlearning_robot/QLearner.py
+++ qlearning_robot/QLearner.py
@@ -9,7 +9,7 @@ class QLearner(object):
 
     """
     
-    Briefly describe!
+    Briefly describe! Bla Bla Bla
 
     --------------------------
     Actions: There are 4 possible actions
@@ -18,9 +18,8 @@ class QLearner(object):
     2 - move south
     3 - move west
     
-    States: Mapping state from 2D to 1D
-    state = icol * 10 + irow 
-   
+    States: Mapping state from 2D to 1D  state = icol * 10 + irow, why on
+    earth they called it descritization, isn't is just mapping?
     """
     
     def __init__(self, num_states=100, num_actions = 4, alpha = 0.2, \
@@ -55,17 +54,31 @@ class QLearner(object):
         """
         @summary: Update the Q table and return an action
         @param s_prime: The new state
-        @param r: The ne state
+        @param r: The new state reward
         @returns: The selected action
         """
-        action = rand.randint(0, self.num_actions-1)
+
+        s = self.s # current state
+        a = self.a # current action
+
+        # update Q-table by looking into future state(s_prime)
+        self.Q[s,a] = (1-self.alpha)*self.Q[s,a] + \
+                           self.alpha*(r + self.gamma * np.max(self.Q[s_prime,:]))  
+        
+        action = np.argmax(self.Q[s_prime,:])  # take best action
+        if rand.uniform(0.0, 1.0) <= self.rar: # take rand action conditionally
+            action = rand.randint(0,3)         # choose the random direction
+        
+        self.rar = self.rar * self.radr        # update rar globally
+        self.s = s_prime                       # update current state globally
+        self.a = action                        # update action globally 
+        
         if self.verbose: print "s =", s_prime,"a =",action,"r =",r
         return action
-
 
     
     def author(self):
         return 'truzmetov3'
 
 if __name__=="__main__":
-    print "Remember Q from Star Trek? Well, this isn't him"
+    print "I'm not fan of Star Trek, but like MCU"
